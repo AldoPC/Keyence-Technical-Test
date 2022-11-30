50fa1afc65d2cfac61a207326f1e37ee09d61c29
AldoPC <aldoponcedelacruz@gmail.com>
2022-11-29 22:05:18 -0600

    added edit and view functionalities

--- js/toDo.js
+++ js/toDo.js
@@ -2,6 +2,7 @@ let enter = document.getElementById("enter");
 let input = document.getElementById("input");
 let list = document.getElementById("output");
 let item = document.getElementById("item");
+let radioOption = document.querySelectorAll("input[name=view_list]");
 let languages = [];
 let keys = localStorage.getItem("counter");
 
@@ -17,7 +18,7 @@ function createListElement() {
   let div = document.createElement("div");
   div.className = "border border-1 rounded min-width me-1 my-2 hovered";
   div.setAttribute("id", "item");
-  localStorage.setItem(keys, input.value);
+  localStorage.setItem("toDo_" + keys, input.value);
   div.appendChild(document.createTextNode(input.value));
   list.appendChild(div);
   input.value = "";
@@ -27,11 +28,25 @@ function createListElement() {
   deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
   deleteButton.appendChild(document.createTextNode("X"));
   div.appendChild(deleteButton);
+  div.addEventListener("click", editListItem);
   deleteButton.addEventListener("click", deleteListItem);
 
   function deleteListItem() {
     div.remove();
     localStorage.removeItem(keys);
+  }
+  function editListItem() {
+    const text = div.textContent.replace("X", "");
+    console.log(text);
+    const keyToUpdate = getKeyByValue(localStorage, text);
+    console.log(keyToUpdate);
+    div.contentEditable = true;
+    div.focus();
+    div.addEventListener("blur", saveListItem);
+    function saveListItem() {
+      div.contentEditable = false;
+      localStorage.setItem(keyToUpdate, div.textContent.replace("X", ""));
+    }
   }
 }
 
@@ -42,28 +57,57 @@ function addOnEnter(event) {
 }
 
 function renderList() {
-  Object.keys(localStorage).forEach(function (key) {
-    if (key === "counter") {
-      return;
-    }
-    let div = document.createElement("div");
-    div.className = "border border-1 rounded min-width me-1 my-2 hovered";
-    div.setAttribute("id", "item");
-    div.appendChild(document.createTextNode(localStorage.getItem(key)));
-    list.appendChild(div);
-    input.value = "";
-    let deleteButton = document.createElement("button");
-    deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
-    deleteButton.appendChild(document.createTextNode("X"));
-    div.appendChild(deleteButton);
-    deleteButton.addEventListener("click", deleteListItem);
+  Object.keys(localStorage)
+    .sort()
+    .forEach(function (key) {
+      if (key.split("_")[0] === "toDo") {
+        let div = document.createElement("div");
+        div.className = "border border-1 rounded min-width me-1 my-2 hovered";
+        div.setAttribute("id", "item");
+        div.appendChild(document.createTextNode(localStorage.getItem(key)));
+        list.appendChild(div);
+        input.value = "";
+        let deleteButton = document.createElement("button");
+        deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
+        deleteButton.appendChild(document.createTextNode("X"));
+        div.appendChild(deleteButton);
+        div.addEventListener("click", editListItem);
+        deleteButton.addEventListener("click", deleteListItem);
 
-    function deleteListItem() {
-      div.remove();
-      localStorage.removeItem(key);
-    }
-  });
+        function deleteListItem() {
+          div.remove();
+          localStorage.removeItem(key);
+        }
+
+        function editListItem() {
+          const text = div.textContent.replace("X", "");
+          console.log(text);
+          const keyToUpdate = getKeyByValue(localStorage, text);
+          console.log(keyToUpdate);
+          div.contentEditable = true;
+          div.focus();
+          div.addEventListener("blur", saveListItem);
+          function saveListItem() {
+            div.contentEditable = false;
+            localStorage.setItem(keyToUpdate, div.textContent.replace("X", ""));
+          }
+        }
+      }
+    });
+}
+
+function renderView() {
+  if (radioOption[0].checked) {
+    document.getElementById("output").style.display = "block";
+  } else if (radioOption[1].checked) {
+    document.getElementById("output").style.display = "flex";
+  }
+}
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
 }
 
 input.addEventListener("keypress", addOnEnter);
+
 window.onload = renderList();
