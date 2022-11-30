faf5e537780aef12a8868f8f20da6f49ba1dd8e3
AldoPC <aldoponcedelacruz@gmail.com>
2022-11-30 00:20:05 -0600

    polished functionalities, code refactoring

--- js/toDo.js
+++ js/toDo.js
@@ -15,39 +15,10 @@ function listLenght() {
 }
 
 function createListElement() {
-  let div = document.createElement("div");
-  div.className = "border border-1 rounded min-width me-1 my-2 hovered";
-  div.setAttribute("id", "item");
   localStorage.setItem("toDo_" + keys, input.value);
-  div.appendChild(document.createTextNode(input.value));
-  list.appendChild(div);
-  input.value = "";
   keys = Number(keys) + 1;
   localStorage.setItem("counter", Number(keys));
-  let deleteButton = document.createElement("button");
-  deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
-  deleteButton.appendChild(document.createTextNode("X"));
-  div.appendChild(deleteButton);
-  div.addEventListener("click", editListItem);
-  deleteButton.addEventListener("click", deleteListItem);
-
-  function deleteListItem() {
-    div.remove();
-    localStorage.removeItem(keys);
-  }
-  function editListItem() {
-    const text = div.textContent.replace("X", "");
-    console.log(text);
-    const keyToUpdate = getKeyByValue(localStorage, text);
-    console.log(keyToUpdate);
-    div.contentEditable = true;
-    div.focus();
-    div.addEventListener("blur", saveListItem);
-    function saveListItem() {
-      div.contentEditable = false;
-      localStorage.setItem(keyToUpdate, div.textContent.replace("X", ""));
-    }
-  }
+  createListComponents(input.value, keys);
 }
 
 function addOnEnter(event) {
@@ -56,51 +27,81 @@ function addOnEnter(event) {
   }
 }
 
+function createListComponents(value, key) {
+  let div = createDiv();
+  createDivText(value, div);
+  createDeleteButon(div, key);
+}
+
 function renderList() {
   Object.keys(localStorage)
     .sort()
     .forEach(function (key) {
       if (key.split("_")[0] === "toDo") {
-        let div = document.createElement("div");
-        div.className = "border border-1 rounded min-width me-1 my-2 hovered";
-        div.setAttribute("id", "item");
-        div.appendChild(document.createTextNode(localStorage.getItem(key)));
-        list.appendChild(div);
-        input.value = "";
-        let deleteButton = document.createElement("button");
-        deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
-        deleteButton.appendChild(document.createTextNode("X"));
-        div.appendChild(deleteButton);
-        div.addEventListener("click", editListItem);
-        deleteButton.addEventListener("click", deleteListItem);
-
-        function deleteListItem() {
-          div.remove();
-          localStorage.removeItem(key);
-        }
-
-        function editListItem() {
-          const text = div.textContent.replace("X", "");
-          console.log(text);
-          const keyToUpdate = getKeyByValue(localStorage, text);
-          console.log(keyToUpdate);
-          div.contentEditable = true;
-          div.focus();
-          div.addEventListener("blur", saveListItem);
-          function saveListItem() {
-            div.contentEditable = false;
-            localStorage.setItem(keyToUpdate, div.textContent.replace("X", ""));
-          }
-        }
+        createListComponents(localStorage.getItem(key), key);
       }
     });
 }
 
+function createDiv() {
+  let div = document.createElement("div");
+  div.className =
+    "border border-1 rounded min-width me-1 my-2 hovered d-flex justify-content-between text-center align-items-center";
+  div.setAttribute("id", "item");
+  return div;
+}
+
+function createDivText(value, div) {
+  let divText = document.createElement("div");
+  divText.appendChild(document.createTextNode(value));
+  div.appendChild(divText);
+  list.appendChild(div);
+  input.value = "";
+  divText.addEventListener("click", function (e) {
+    editListItem(e);
+  });
+  return divText;
+}
+
+function createDeleteButon(div, key) {
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "btn btn-danger btn-sm mx-2 my-2";
+  deleteButton.appendChild(document.createTextNode("X"));
+  div.appendChild(deleteButton);
+  deleteButton.addEventListener("click", function () {
+    deleteListItem(div, key);
+  });
+}
+
+function deleteListItem(div, key) {
+  debugger;
+  div.remove();
+  localStorage.removeItem(key);
+}
+
+function editListItem(event) {
+  let divText = event.target;
+  const text = divText.textContent;
+  const keyToUpdate = getKeyByValue(localStorage, text);
+  divText.contentEditable = true;
+  divText.focus();
+  divText.addEventListener("blur", saveListItem);
+  divText.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+      divText.contentEditable = false;
+    }
+  });
+  function saveListItem() {
+    divText.contentEditable = false;
+    localStorage.setItem(keyToUpdate, divText.textContent);
+  }
+}
+
 function renderView() {
   if (radioOption[0].checked) {
-    document.getElementById("output").style.display = "block";
-  } else if (radioOption[1].checked) {
     document.getElementById("output").style.display = "flex";
+  } else if (radioOption[1].checked) {
+    document.getElementById("output").style.display = "block";
   }
 }
 
